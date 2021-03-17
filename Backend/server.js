fd4d824a9cbf7d833edef142e5077a8f7c38a0c9
arce <arcegutierrez.2508@gmail.com>
2021-03-17 15:05:54 -0600

    actualizacion de prueba

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+
+const express = require('express')
+const cors = require('cors')
+const userRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+server.use('/users', userRouter)
+server.use('/auth', authRouter)
+
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  console.error(err)
+
+  res.status(500).json({ error: err.message })
+}
+
+server.use(errorHandler)
+
+module.exports = server
